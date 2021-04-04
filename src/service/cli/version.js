5bf1eeaf35e09a5a103835dfff3930d87aeabeec
Viacheslav Lushchinskiy <slavalu74@gmail.com>
2021-04-04 18:24:54 +0200

    задание выполнено

---
+++ src/service/cli/version.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const {version} = require(`../../../package.json`);
+
+module.exports = {
+  name: `--version`,
+  run: () => {
+    console.info(`version is: `, version);
+  }
+};
+
