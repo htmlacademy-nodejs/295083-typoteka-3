c7cce9f30508d01a4b704c5728026e0809cdff0d
Viacheslav Lushchinskiy <slavalu74@gmail.com>
2021-06-15 12:59:45 +0200

    Шаблонизируй это

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,8 +4,8 @@ const {Router} = require(`express`);
 
 const articlesRoutes = new Router();
 
-articlesRoutes.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRoutes.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
+articlesRoutes.get(`/add`, (req, res) => res.render(`new-post`));
+articlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 articlesRoutes.get(`/:id`, (req, res) => res.send(`/articles/:id`));
 articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
 
