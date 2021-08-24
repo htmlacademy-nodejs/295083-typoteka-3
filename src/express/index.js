2d35f258b1d8c4357d0d7f0081dc2420028be445
Viacheslav Lushchinskiy <slavalu74@gmail.com>
2021-08-24 23:10:48 +0200

    adding the new article

--- src/express/index.js
+++ src/express/index.js
@@ -3,6 +3,7 @@
 const express = require(`express`);
 const path = require(`path`);
 const PUBLIC_DIR = `public`;
+const UPLOAD_DIR = `upload`;
 
 const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
@@ -12,6 +13,7 @@ const DEFAULT_PORT = 8080;
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
 
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -2,13 +2,34 @@
 
 const {Router} = require(`express`);
 const api = require(`../api`).getAPI();
+const multer = require(`multer`);
+const path = require(`path`);
+const {nanoid} = require(`nanoid`);
 
+const UPLOAD_DIR = `../upload/img/`;
+const uploadDirAbsolute = path.resolve(__dirname, UPLOAD_DIR);
 
 const articlesRoutes = new Router();
 
-articlesRoutes.get(`/add`, (req, res) => res.render(`new-post`));
+const storage = multer.diskStorage({
+  destination: uploadDirAbsolute,
+  filename: (req, file, cb) => {
+    const uniqueName = nanoid(10);
+    const extension = file.originalname.split(`.`).pop();
+    cb(null, `${uniqueName}.${extension}`);
+  }
+});
+
+const upload = multer({storage});
+
+articlesRoutes.get(`/add`, async (req, res) => {
+  const categories = await api.getCategories();
+  res.render(`new-post`, {categories});
+});
+
 articlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
-articlesRoutes.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+
+articlesRoutes.get(`/:id`, (req, res) => res.render(`post`));
 
 
 articlesRoutes.get(`/edit/:id`, async (req, res) => {
@@ -17,5 +38,26 @@ articlesRoutes.get(`/edit/:id`, async (req, res) => {
   res.render(`edit-post`, {article, categories});
 });
 
+articlesRoutes.post(`/add`, upload.single(`upload`), async (req, res) => {
+  const {body, file} = req;
+  const articleData = {
+    title: body.title,
+    announce: body.announcement,
+    fullText: body.fullText,
+    createdDate: new Date().toISOString(),
+    category: Array.isArray(body.category) ? body.category : [body.category]
+  };
+  if (file) {
+    articleData.photo = file.filename;
+  }
+
+
+  try {
+    await api.createArticle(articleData);
+    res.redirect(`/my`);
+  } catch (error) {
+    res.redirect(`back`);
+  }
+});
 
 module.exports = articlesRoutes;
