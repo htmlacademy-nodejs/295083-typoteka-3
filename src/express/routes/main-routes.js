ec346877a6fff67db95d6e9030654a1e1e0e590b
Viacheslav Lushchinskiy <slavalu74@gmail.com>
2021-08-25 20:55:28 +0200

    search done

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -11,7 +11,18 @@ mainRoutes.get(`/`, async (req, res) => {
 });
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
-mainRoutes.get(`/search`, (req, res) => res.render(`search`));
+
+mainRoutes.get(`/search`, async (req, res) => {
+  const {search} = req.query;
+  try {
+    const results = search ? await api.search(search) : [];
+
+    res.render(`search`, {results});
+  } catch (error) {
+    res.render(`search`, {results: []});
+  }
+});
+
 mainRoutes.get(`/categories`, (req, res) => res.render(`all-categories`));
 
 module.exports = mainRoutes;
